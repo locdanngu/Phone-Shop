8f64c999a177c19c9a0d95b03e3c2b9be4cfcd43
Trần Văn Lộc <tranvanloc96lhp@gmail.com>
2023-09-11 11:04:33 +0700

    up

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,21 @@
+<?php
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('admin')->insert([
+            'adminname' => 'admin',
+            'password' => Hash::make('123456'), // Hãy thay thế bằng mật khẩu thật
+            'role' => 'admin',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+}
+

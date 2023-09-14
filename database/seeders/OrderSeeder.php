f76ef20722321b662a0cb8077161d4dbf36ab9d0
Trần Văn Lộc <tranvanloc96lhp@gmail.com>
2023-09-14 15:24:11 +0700

    up

---
+++ database/seeders/OrderSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class OrderSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('orders')->insert([
+            'iduser' =>  1,
+            'status' => 'done',
+            'totalprice' => 2000,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('orders_product')->insert([
+            'idorder' => 1,
+            'idproduct' => 1,
+            'quantity' => 4,
+        ]);
+    }
+}

bf79ad633ddaea7a15da883f5b1deedcbac8ebd2
Trần Văn Lộc <tranvanloc96lhp@gmail.com>
2023-09-15 10:12:13 +0700

    up

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Providers;
-
+use Illuminate\Support\Facades\View;
+use App\Models\Order; // Import model Friend
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,22 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        View::composer(['admin/page/Changepasswordpage', 
+                        'admin/page/Homepage', 
+                        'admin/page/Listcategorypage', 
+                        'admin/page/Listcouponpage', 
+                        'admin/page/Listorderpage',
+                        'admin/page/Listproductpage',], function ($view) {
+                            
+                            
+            $countorder = Order::where('status', 'wait')->count();
+
+            
+            $view->with([
+                'countorder' => $countorder,
+                
+            ]);
+            
+        });
     }
 }

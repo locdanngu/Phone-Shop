d8e40a31e744fe404b6248e879d702b38717364e
Trần Văn Lộc <tranvanloc96lhp@gmail.com>
2023-09-18 09:43:05 +0700

    up

---
+++ resources/views/user/layouts/Linkscript.blade.php
@@ -0,0 +1,19 @@
+<!-- Latest jQuery form server -->
+<script src="https://code.jquery.com/jquery.min.js"></script>
+
+<!-- Bootstrap JS form CDN -->
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+
+<!-- jQuery sticky menu -->
+<script src="bootstrap/owl.carousel.min.js"></script>
+<script src="bootstrap/jquery.sticky.js"></script>
+
+<!-- jQuery easing -->
+<script src="bootstrap/jquery.easing.1.3.min.js"></script>
+
+<!-- Main Script -->
+<script src="bootstrap/main.js"></script>
+
+<!-- Slider -->
+<script type="text/javascript" src="bootstrap/bxslider.min.js"></script>
+<script type="text/javascript" src="bootstrap/script.slider.js"></script>
--- resources/views/user/layouts/Userlayout.blade.php
+++ resources/views/user/layouts/Userlayout.blade.php
@@ -5,51 +5,24 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Ustora Demo</title>
-
-    <!-- Google Fonts -->
-    <link href='http://fonts.googleapis.com/css?family=Titillium+Web:400,200,300,700,600' rel='stylesheet'
-        type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:400,700,300' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Raleway:400,100' rel='stylesheet' type='text/css'>
-
-    <!-- Bootstrap -->
-    <link rel="stylesheet" href="bootstrap/bootstrap.min.css">
-
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="bootstrap/font-awesome.min.css">
-
-    <!-- Custom CSS -->
-    <link rel="stylesheet" href="bootstrap/owl.carousel.css">
-    <link rel="stylesheet" href="stylesheets/User.css">
-    <link rel="stylesheet" href="bootstrap/responsive.css">
+    <title>@yield('title')</title>
+    @include('user.layouts.Linkuser')
 </head>
 
 <body>
+    @include('user.layouts.Headeruser')
+
+
+    @yield('body')
 
 
 
-    
+    @include('user.layouts.Footeruser')
 
-    <!-- Latest jQuery form server -->
-    <script src="https://code.jquery.com/jquery.min.js"></script>
+    @include('admin.layouts.Linkscript')
+    @yield('popup')
+    @yield('js')
 
-    <!-- Bootstrap JS form CDN -->
-    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-
-    <!-- jQuery sticky menu -->
-    <script src="js/owl.carousel.min.js"></script>
-    <script src="js/jquery.sticky.js"></script>
-
-    <!-- jQuery easing -->
-    <script src="js/jquery.easing.1.3.min.js"></script>
-
-    <!-- Main Script -->
-    <script src="js/main.js"></script>
-
-    <!-- Slider -->
-    <script type="text/javascript" src="js/bxslider.min.js"></script>
-    <script type="text/javascript" src="js/script.slider.js"></script>
 </body>
 
 </html>

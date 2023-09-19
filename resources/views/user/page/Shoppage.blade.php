ef69716fd0332a2cd662d5780d89e99f2423dd24
Trần Văn Lộc <tranvanloc96lhp@gmail.com>
2023-09-19 16:06:59 +0700

    up

--- resources/views/user/page/Shoppage.blade.php
+++ resources/views/user/page/Shoppage.blade.php
@@ -37,8 +37,8 @@
                         </div>
 
                         <div class="product-option-shop">
-                            <a class="add_to_cart_button" data-quantity="1" data-product_sku="" data-product_id="70"
-                                rel="nofollow" href="/canvas/shop/?add-to-cart=70">Add to cart</a>
+                            <a class="add_to_cart_button them-sp-vao-gio" href="#"
+                                data-idproduct="{{ $pr->idproduct }}">Add to cart</a>
                         </div>
                     </div>
                 </div>

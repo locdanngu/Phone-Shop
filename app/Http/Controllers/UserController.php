2be26c6f994d58a8f3fcbf99c8766c011ad05e68
Trần Văn Lộc <tranvanloc96lhp@gmail.com>
2023-09-19 12:02:01 +0700

    up

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -153,26 +153,17 @@ class UserController extends Controller
 
     public function registeruser(Request $request)
     {
-        // $input = $request->all();
-        // $user = User::where('email', $input['email'])->first();
-        // if ($request->password !== $request->repassword) {
-        //     return redirect()->back()->withInput()->withErrors(['password' => 'Mật khẩu nhập lại không khớp']);
-        // }else if($user){
-        //     return redirect()->back()->withInput()->withErrors(['email' => 'Email tài khoản đã tồn tại']);
-        // }else{
-        //     $user = User::create([
-        //         'email' => $input['email'],
-        //         'phone' => $input['phone'],
-        //         'name' => $input['name'],
-        //         'password' => Hash::make($input['password']),
-        //         'avatar' => '/images/avataradmin.png',
-        //     ]);
-        //     // // Đăng nhập người dùng mới đăng ký
-        //     Auth::login($user);
-        //     // Chuyển hướng sang trang Userpage, truyền thông tin người dùng qua biến user
-        //     return redirect()->route('adminlogin.page');
-        //     // return view('/Codesms', ['input' => $input, 'random_number' => $random_number]);
-        // }
+        $input = $request->all();
+        $user = User::create([
+            'email' => $input['email'],
+            'phone' => $input['phone'],
+            'firstname' => $input['firstname'],
+            'lastname' => $input['lastname'],
+            'username' => $input['username'],
+            'password' => Hash::make($input['password']),
+        ]);
+        Auth::login($user);
+
         return redirect()->route('home.page');
     }
 

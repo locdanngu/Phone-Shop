916ad7635de02d2b33d56c7847bd67140482b32a
Trần Văn Lộc <tranvanloc96lhp@gmail.com>
2023-09-22 15:40:32 +0700

    up

---
+++ app/Models/Contact.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Contact extends Model
+{
+    protected $table = 'contact';
+    protected $primaryKey = 'idcontact'; // Tên trường khóa chính
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+        'content',
+    ];
+
+   
+}

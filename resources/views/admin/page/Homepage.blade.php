df968e91561032bd0162f035bbeeda9431dc8eb2
Trần Văn Lộc <tranvanloc96lhp@gmail.com>
2023-09-24 19:49:19 +0700

    up

--- resources/views/admin/page/Homepage.blade.php
+++ resources/views/admin/page/Homepage.blade.php
@@ -114,7 +114,7 @@
         <div class="row">
 
             <!-- Area Chart -->
-            <div class="col-xl-12 col-lg-7">
+            <div class="col-xl-6 col-lg-7">
                 <div class="card shadow mb-4">
                     <!-- Card Header - Dropdown -->
                     <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
@@ -132,17 +132,7 @@
             </div>
 
             <!-- Pie Chart -->
-        </div>
-    </div>
-    <div class="container-fluid">
-
-
-        <div class="row">
-
-            <!-- Area Chart -->
-
-            <!-- Pie Chart -->
-            <div class="col-xl-12 col-lg-5">
+            <div class="col-xl-6 col-lg-7">
                 <div class="card shadow mb-4">
                     <!-- Card Header - Dropdown -->
                     <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">

d2ee117e68e1459545aa90b15e38206171ea0053
Trần Văn Lộc <tranvanloc96lhp@gmail.com>
2023-09-27 14:46:46 +0700

    up

--- resources/views/user/page/Productpage.blade.php
+++ resources/views/user/page/Productpage.blade.php
@@ -8,7 +8,7 @@
         <div class="row">
             <div class="col-md-12">
                 <div class="product-bit-title text-center">
-                    <h2>Shop</h2>
+                    <h2>{{ trans('messages.product') }}</h2>
                 </div>
             </div>
         </div>
